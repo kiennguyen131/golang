87df8904ad28621cd075b2d5e7b49e327b800244
Nguyen Trung Kien 4 <Kien4.NguyenTrung@mbs.com.vn>
2024-08-09 17:22:49 +0700

    bai 12

--- global/global.go
+++ global/global.go
@@ -3,9 +3,12 @@ package global
 import (
 	"ecommerce-backend-api/init/pkg/logger"
 	"ecommerce-backend-api/init/pkg/setting"
+
+	"gorm.io/gorm"
 )
 
 var (
 	Config setting.Config
 	Logger *logger.LoggerZap
+	Mdb    *gorm.DB
 )
